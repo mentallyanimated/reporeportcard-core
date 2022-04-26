08016efd04777bd146fbfb78b60af9c87239dfa9
Aaron Batilo <aaron.batilo@color.com>
2022-04-26 09:37:26 -0700

    Set 2 month download as default

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"os"
+	"time"
 
+	"github.com/mentallyanimated/reporeportcard-core/github"
+	"github.com/mentallyanimated/reporeportcard-core/graph"
 	"github.com/mentallyanimated/reporeportcard-core/server"
+	"github.com/mentallyanimated/reporeportcard-core/store"
 )
 
 func main() {
 	ownerFlag := flag.String("owner", "mentallyanimated", "The owner of the repository")
 	repoFlag := flag.String("repo", "reporeportcard-core", "The repository to analyze")
+	serveFlag := flag.Bool("serve", false, "Set to true to serve the API")
+	durationFlag := flag.Duration("duration", 60*24*time.Hour, "The duration of the analysis")
 	flag.Parse()
 
 	if *ownerFlag == "" || *repoFlag == "" {
@@ -17,22 +24,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	// owner := *ownerFlag
-	// repo := *repoFlag
+	if *serveFlag {
+		server := server.NewServer()
+		server.Start()
+	} else {
+		owner := *ownerFlag
+		repo := *repoFlag
 
-	// ctx, cancel := context.WithCancel(context.Background())
-	// defer cancel()
+		cache := store.NewDisk(owner, repo)
 
-	// cache := store.NewDisk(owner, repo)
-	// client := github.NewClient(ctx, os.Getenv("GITHUB_TOKEN"), cache, owner, repo)
-	// client.DownloadPullDetails(ctx)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-	server := server.NewServer()
-	server.Start()
+		client := github.NewClient(ctx, os.Getenv("GITHUB_TOKEN"), cache, owner, repo)
+		client.DownloadPullDetails(ctx)
 
-	// pullDetails := graph.ImportRawData(owner, repo)
-	// filteredPullDetails := graph.FilterPullDetailsByTime(pullDetails, time.Unix(0, 0), time.Now())
-	// // filteredPullDetails := graph.FilterPullDetailsByTime(pullDetails, time.Now().Add(-time.Hour*24*365), time.Now())
+		pullDetails := graph.ImportRawData(owner, repo)
+		filteredPullDetails := graph.FilterPullDetailsByTime(pullDetails, time.Now().Add(-*durationFlag), time.Now())
 
-	// graph.BuildForceGraph(owner, repo, filteredPullDetails, os.Stdout)
+		graph.BuildForceGraph(owner, repo, filteredPullDetails, os.Stdout)
+	}
 }
